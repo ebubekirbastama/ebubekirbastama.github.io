eb2f1c291a74e2d4929e86b6b7e1ae3866a7f864
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2024-07-17 16:01:46 +0300

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,56 @@
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const recordedVideo = document.getElementById('recordedVideo');
+
+let mediaRecorder;
+let recordedChunks = [];
+
+startBtn.addEventListener('click', async () => {
+    const displayStream = await navigator.mediaDevices.getDisplayMedia({
+        video: { mediaSource: 'screen' }
+    });
+
+    const audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: true
+    });
+
+    const combinedStream = new MediaStream([
+        ...displayStream.getVideoTracks(),
+        ...audioStream.getAudioTracks()
+    ]);
+
+    mediaRecorder = new MediaRecorder(combinedStream);
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start();
+
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+    mediaRecorder.stop();
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+});
+
+function handleDataAvailable(event) {
+    if (event.data.size > 0) {
+        recordedChunks.push(event.data);
+        download();
+    }
+}
+
+function download() {
+    const blob = new Blob(recordedChunks, {
+        type: 'video/webm'
+    });
+    const url = URL.createObjectURL(blob);
+    recordedVideo.src = url;
+    const a = document.createElement('a');
+    document.body.appendChild(a);
+    a.style = 'display: none';
+    a.href = url;
+    a.download = 'screen-recording.webm';
+    a.click();
+    window.URL.revokeObjectURL(url);
+}
